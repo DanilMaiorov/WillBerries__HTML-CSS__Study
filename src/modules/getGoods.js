e9f8e262fcb7b5ef067fa948e0b97ccd25b2751b
Danil Maiorov <damayorov93@gmail.com>
2022-02-27 23:10:32 +0500

    before merge

--- src/modules/getGoods.js
+++ src/modules/getGoods.js
@@ -1,7 +1,6 @@
 export function getGoods () {
     const links = document.querySelectorAll('.navigation__item-link')
-
-    
+        
     function getData () {
         fetch('https://willberries-test-e8a5b-default-rtdb.firebaseio.com/db.json')
             .then(res => {
